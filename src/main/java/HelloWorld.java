96fda2fbc2aa8c9601ec49e072ed72cea56d8748
Rakesh Kumar <rakeshkumarkurakula4@gmail.com>
2026-04-05 16:08:09 -0500

    Convert app to web server

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        while (true) {
-            System.out.println("Hello World - Running...");
-            Thread.sleep(10000);
-        }
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", (exchange) -> {
+            String response = "Hello from DevOps App 🚀";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080...");
     }
 }
 // test
